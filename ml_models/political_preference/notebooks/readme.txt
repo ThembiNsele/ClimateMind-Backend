27242f98c25a70721368f461f824f27c71e64a7d
Alexis Carras <alexisjcarras@gmail.com>
2021-03-28 11:27:04 +0200

    ml_models include

---
+++ ml_models/political_preference/notebooks/readme.txt
@@ -0,0 +1,1 @@
+Notebooks used for data exploration and experimentation for the political preference prediction task
---
+++ ml_models/political_preference/src/political_preference.py
@@ -0,0 +1,28 @@
+from data_processor import *
+from model import *
+
+
+#print("imblearn version", imblearn.__version__)
+
+def main():
+
+    #Radical - conservative run
+    conservatives = DataProcessor("../datasets/pvq21_REVERSED_CENTRED.csv", 'conservative', 7)
+    X_train, X_test, y_train, y_test = conservatives.split_data()
+
+    RF_con_model = Model("LASSO", "conservative", (X_train, y_train), (X_test, y_test), "../models/RandomForest_conservative_0.739")
+    RF_con_model.test()
+    #RF_con_model.store_model()
+
+    #Radical - liberal run
+    liberals = DataProcessor("../datasets/pvq21_REVERSED_CENTRED.csv", 'liberal', 3)
+    X_train, X_test, y_train, y_test = liberals.split_data()
+
+    RF_lib_model = Model("LASSO", "liberal", (X_train, y_train), (X_test, y_test), "../models/RandomForest_liberal_0.783")
+    RF_lib_model.test()
+    #RF_lib_model.store_model()
+
+
+if __name__ == "__main__":
+    main()
+
