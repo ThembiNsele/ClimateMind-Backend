29b77d495932f8b259ab1716933632994168074e
Henry Nguyen <henry2833@gmail.com>
2021-04-03 19:20:45 -0700

    CM-601 Deleted extra expected_scores variable.

--- tests/scoring/test_scoring.py
+++ tests/scoring/test_scoring.py
@@ -22,19 +22,6 @@ def test_persist_scores(client):
         "power": 1.0,
     }
 
-    expected_scores = Scores()
-    expected_scores.session_uuid = test_data["session-id"]
-    expected_scores.security = test_data["security"]
-    expected_scores.conformity = test_data["conformity"]
-    expected_scores.benevolence = test_data["benevolence"]
-    expected_scores.tradition = test_data["tradition"]
-    expected_scores.universalism = test_data["universalism"]
-    expected_scores.self_direction = test_data["self-direction"]
-    expected_scores.stimulation = test_data["stimulation"]
-    expected_scores.hedonism = test_data["hedonism"]
-    expected_scores.achievement = test_data["achievement"]
-    expected_scores.power = test_data["power"]
-
     with patch('app.scoring.persist_scores.db') as fake_db:
         persist_scores(test_data)
         session_has_been_added = False
